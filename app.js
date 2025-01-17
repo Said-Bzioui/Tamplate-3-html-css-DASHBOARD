833e9984d511461ee851e55ab9b3a5438ad3b865
SAID BZIOUI <saidbzioui20@gmail.com>
2025-01-17 09:01:57 +0100

    'V1'

---
+++ app.js
@@ -0,0 +1,10 @@
+let notifiBtn = document.querySelector('.notifi');
+
+let notifiBar = document.querySelector(".notifi-bar");
+let mood = document.querySelector('.mood');
+
+notifiBtn.onclick = () => {
+    notifiBar.classList.toggle('show');
+}
+
+
